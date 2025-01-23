e550307454806f166068da0d6b9f88146940a235
alejaandroobc <abotellacosta2@gmail.com>
2025-01-23 10:32:56 +0100

    Inicio del proyecto!

---
+++ src/main/java/com/proyecto/ManejoDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManejoDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManejoDeTareasApplication.class, args);
+	}
+
+}
